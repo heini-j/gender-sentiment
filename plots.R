44e0bfbcf91f8dad77dc69435c40524b8e79ae2c
Heini Järviö <heinimjarv@gmail.com>
2025-02-21 13:53:25 +0100

    tried ridgeplots

--- plots.R
+++ plots.R
@@ -1,6 +1,9 @@
 library(readr)
 library(dplyr)
 library(lubridate)
+library(ridgeline)
+library(ggridges)
+library(ggplot2)
 
 # Reading and cleaning the data -------------------------------------------
 
@@ -31,6 +34,14 @@ sentiment_df$date <- dmy(sentiment_df$date)
 sentiment_df$round1 <- ifelse(sentiment_df$date < dmy("01-12-2022"), 1, 0)
 sentiment_df$round2 <- ifelse(sentiment_df$date >= dmy("01-12-2022") & sentiment_df$date < dmy("01-12-2023"), 1, 0)
 sentiment_df$round3 <- ifelse(sentiment_df$date >= dmy("01-12-2023"), 1, 0)
+
+# Creating a variable "round" that is 1, 2 or 3 based on the date
+
+sentiment_df$round <- case_when(
+  sentiment_df$date < dmy("01-12-2022") ~ 1,
+  sentiment_df$date >= dmy("01-12-2022") & sentiment_df$date < dmy("01-12-2023") ~ 2,
+  sentiment_df$date >= dmy("01-12-2023") ~ 3
+)
 
 # Creating a new column for the gender of the politician
 
@@ -66,4 +77,71 @@ p2 <- ggplot(sentiment_df, aes(mean_sentiment)) +
     x = "sentiment score average per article",
     y = "count",
   )
+
+ggplot(sentiment_df, aes(x = mean_sentiment, fill = gender)) +
+  facet_wrap(~ paper, scales = "free_x", ncol = 2) +  
+  geom_density(alpha = 0.3) +
+  theme_minimal()
+
+
+# making a function to create the histogram for each code
+
+sentiment_df |>
+  filter(gender == "female") |>
+  ggplot(aes(mean_sentiment)) +
+  geom_histogram(binwidth = 0.5) +
+  facet_wrap(paper~code, scales = "free_y") +
+  labs(
+    title = "Distribution of mean sentiment scores per female politician",
+    x = "Mean sentiment score",
+    y = "Frequency"
+  ) +
+  theme_minimal()
+
+sentiment_df |>
+  filter(gender == "male") |>
+  ggplot(aes(mean_sentiment)) +
+  geom_histogram(binwidth = 0.5) +
+  facet_grid(code ~ paper, scales = "free_y") +
+  labs(
+    title = "Distribution of mean sentiment scores per female politician",
+    x = "Mean sentiment score",
+    y = "Frequency"
+  ) +
+  theme_minimal()
+
+  
+
+create_histogram <- function(code) {
+    sentiment_df |>
+    filter(code == code) |>
+    ggplot(aes(mean_sentiment)) +
+    geom_histogram(binwidth = 0.5) +
+    facet_wrap(~paper) +
+    labs(
+      title = paste("Distribution of mean sentiment scores for", code),
+      x = "Mean sentiment score",
+      y = "Frequency"
+    )
+}
+
+codes <- c("EH", "BHK", "LM", "LS", "VA", "KKS", "EBS", "MR", "MM", "IK",
+           "AC", "TB", "GP", "IC", "AR", "EN", "TA", "GPF", "MC", "JG")
+
+for (code in codes) {
+  create_histogram(code)
+  print(paste("Histogram for code", code, "created"))
+  
+}
+
+# ridgeline plots
+
+ridgeline(x = sentiment_df$mean_sentiment, y = sentiment_df$code, bw = 0.5, mode = TRUE)
+
+sentiment_df |>
+  ggplot(aes(x = mean_sentiment, y = code, fill = gender)) +
+  geom_density_ridges(scale = 1.5, ) +
+  theme_ridges()
+
+
 
