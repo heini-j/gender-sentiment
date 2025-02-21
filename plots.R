4e23ec588009434564bf7ba74edc8a5f257425b5
Heini Järviö <heinimjarv@gmail.com>
2025-02-21 11:04:34 +0100

    added grid plot

--- plots.R
+++ plots.R
@@ -1,6 +1,6 @@
 library(readr)
 library(dplyr)
-
+library(lubridate)
 
 # Reading and cleaning the data -------------------------------------------
 
@@ -22,6 +22,16 @@ sentiment_df <- sentiment_df |>
     title = "Title of article"
   )
 
+# creating round 1, round 2 and round 3 variables based on the date
+
+sentiment_df$date <- dmy(sentiment_df$date)
+
+# creating a round1 variable if the date is before 1-12-2022
+
+sentiment_df$round1 <- ifelse(sentiment_df$date < dmy("01-12-2022"), 1, 0)
+sentiment_df$round2 <- ifelse(sentiment_df$date >= dmy("01-12-2022") & sentiment_df$date < dmy("01-12-2023"), 1, 0)
+sentiment_df$round3 <- ifelse(sentiment_df$date >= dmy("01-12-2023"), 1, 0)
+
 # Creating a new column for the gender of the politician
 
 sentiment_df <- sentiment_df |>
@@ -31,4 +41,29 @@ sentiment_df <- sentiment_df |>
                     "female", "male")
   )
 
+
+# Plotting the sentiment scores -------------------------------------------
+
+p1 <- ggplot(sentiment_df, aes(mean_sentiment)) +
+  geom_histogram(binwidth = 0.5) +
+  facet_grid(gender ~ paper, scales = "free_x", space = "free_x") +
+  labs(
+    title = "sentiment scores",
+    tag = "Figure 1",
+    x = "sentiment score average per article",
+    y = "count",
+  )
+
+p1 + theme_minimal()
+
+
+p2 <- ggplot(sentiment_df, aes(mean_sentiment)) +
+  geom_histogram(binwidth = 0.5) +
+  facet_grid( ~ paper, scales = "free_x", space = "free_x") +
+  labs(
+    title = "sentiment scores",
+    tag = "Figure 1",
+    x = "sentiment score average per article",
+    y = "count",
+  )
 
