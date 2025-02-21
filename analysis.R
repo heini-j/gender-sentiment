b1e77c150db805aaaaa198d67e1240c2bf29eb7d
Heini Järviö <heinimjarv@gmail.com>
2025-02-21 14:05:22 +0100

    tried some analysis

---
+++ analysis.R
@@ -0,0 +1,16 @@
+library(car)
+
+ols_model <- lm(mean_sentiment ~ gender * paper + round, data = sentiment_df)
+summary(ols_model)
+
+
+anova_model <- aov(mean_sentiment ~ gender + paper, data = sentiment_df)
+summary(anova_model)
+
+TukeyHSD(anova_model)
+
+plot(anova_model)  # Residual plots
+shapiro.test(residuals(anova_model))  # Normality test
+leveneTest(anova_model) 
+
+boxplot(residuals(anova_model))
