45671759fd7011789dc6f41307a134f580ce315a
Heini Järviö <heinimjarv@gmail.com>
2025-02-22 17:32:26 +0100

    cleaned code

--- df_building.R
+++ df_building.R
@@ -1,14 +1,11 @@
 library(readr)
 library(tidyverse)
 
+
+# Creating an example case for generalising the code ----------------------------
+
 # Read the CSV file
 df <- read_csv("GPF.csv")
-
-?read_csv
-
-df_wide |>
-  dplyr::summarise(n = dplyr::n(), .by = c(Paper, Date, `Title of article`, variable)) |>
-  dplyr::filter(n > 1L) 
 
 # Transform data to wide format
 
@@ -21,26 +18,6 @@ View(df_wide)
 # adding the code of the person as a column to the new df
 
 df_wide$code <- "GPF"
-
-# recoding all NULL to NA in df_wide
-
-df_wide <- df_wide %>%
-  select(starts_with("sentiment_")) |>
-  mutate(across(everything(), ~ map_dbl(., ~ ifelse(length(.) == 0, NA, as.numeric(.)))))
-         
-# creating a variable that is the mean of all sentiment colums
-
-df_wide$mean_sentiment <- rowMeans(df_wide %>% select(starts_with("sentiment_")), na.rm = TRUE)
-
-# plotting the frequency of the mean sentiment
-
-df_wide %>%
-  ggplot(aes(mean_sentiment)) +
-  geom_histogram(binwidth = 0.5) +
-  facet_wrap(~Paper) +
-  labs(title = "Distribution of mean sentiment scores",
-       x = "Mean sentiment score",
-       y = "Frequency")
 
 
 # creating a function to use it with all the sentiment dfs ------------------------ 
@@ -80,12 +57,62 @@ df_combined <- bind_rows(df_list)
 
 df_combined <- bind_rows(list(df_combined, df_wide))
 
-?bind_rows
-
-
-df_combined$mean_sentiment <- rowMeans(df_combined |> select(starts_with("sentiment_")), na.rm = TRUE)
+# Checking that all codes are included
 
 unique(df_combined$code)
-# saving the dataset
+
+# Cleaning and arrranging the data --------------------------------------------
+
+# Adding a mean sentiment column
+df_combined$mean_sentiment <- rowMeans(df_combined |> select(starts_with("sentiment_")), na.rm = TRUE)
+
+# Moving the code to the beginning of the dataframe
+
+df_combined <- df_combined |>
+  relocate(code)
+
+# Renaming some columns for simplicity
+
+df_combined <- df_combined |>
+  rename(
+    date = Date,
+    paper = Paper,
+    title = "Title of article"
+  )
+
+# creating round 1, round 2 and round 3 variables based on the date
+
+df_combined$date <- dmy(df_combined$date)
+
+# creating a round1 variable if the date is before 1-12-2022
+
+df_combined$round1 <- ifelse(df_combined$date < dmy("01-12-2022"), 1, 0)
+df_combined$round2 <- ifelse(df_combined$date >= dmy("01-12-2022") & df_combined$date < dmy("01-12-2023"), 1, 0)
+df_combined$round3 <- ifelse(df_combined$date >= dmy("01-12-2023"), 1, 0)
+
+# Creating a variable "round" that is 1, 2 or 3 based on the date
+
+df_combined$round <- case_when(
+  df_combined$date < dmy("01-12-2022") ~ 1,
+  df_combined$date >= dmy("01-12-2022") & df_combined$date < dmy("01-12-2023") ~ 2,
+  df_combined$date >= dmy("01-12-2023") ~ 3
+)
+
+# Creating a new column for the gender of the politician
+
+df_combined <- df_combined |>
+  mutate(
+    gender = ifelse(code %in% 
+                      c("EH", "BHK", "LM", "LS", "VA", "KKS", "EBS", "MR", "MM", "IK"), 
+                    "female", "male")
+  )
+
+# changing the type of all columns starting "sentiment_" to numeric
+
+df_combined <- df_combined |>
+  mutate(across(starts_with("sentiment_"), as.numeric))
+
+
+# saving the dataset ----------------------------------------------------------
 
 write_csv(df_combined, "df_combined.csv")
