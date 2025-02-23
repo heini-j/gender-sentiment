16c5b253f1852587155583fa706325d1719a8e57
Heini Järviö <heinimjarv@gmail.com>
2025-02-23 21:15:50 +0100

    cleaned data

--- analysis.R
+++ analysis.R
@@ -1,26 +1,30 @@
 library(car)
 library(readr)
+library(broom)
+library(effects)
 
 sentiment_df <- read_csv("df_combined.csv")
 
-# t-test
+# t-test -----------------------------------------------------------
 
 t.test(mean_sentiment ~ gender, data = sentiment_df)
 
+# OLS regression -----------------------------------------------------------
 
-ols_model <- lm(mean_sentiment ~ gender * paper + round, data = sentiment_df)
+ols_model <- lm(mean_sentiment ~ gender * paper, data = sentiment_df)
 summary(ols_model)
 
+model_summary <- tidy(ols_model)
 
-anova_model <- aov(mean_sentiment ~ gender * paper, data = sentiment_df)
-summary(anova_model)
+effect_plot <- allEffects(ols_model)
+plot(effect_plot)
 
-TukeyHSD(anova_model)
+# plotting the OLS results -----------  ------------------------------
 
-plot(anova_model)  # Residual plots
-shapiro.test(residuals(anova_model))  # Normality test
-leveneTest(anova_model)
+ggplot(model_summary, aes(x = term, y = estimate, ymin = conf.low, ymax = conf.high)) +
+  geom_point() +
+  geom_errorbar(width = 0.2) +
+  coord_flip() +  # Flip the coordinates for better readability
+  labs(title = "Coefficient Plot", x = "Predictors", y = "Estimated Coefficients") +
+  theme_minimal()
 
-boxplot(residuals(anova_model))
-
-kruskal.test(mean_sentiment ~ gender, data = sentiment_df)
