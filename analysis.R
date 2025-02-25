7b4d8b63f2da59aeeda0b07530377e4782fe023a
Heini Järviö <heinimjarv@gmail.com>
2025-02-25 15:00:29 +0100

    added interaction plot

--- analysis.R
+++ analysis.R
@@ -4,8 +4,18 @@ library(broom)
 library(ggeffects)
 library(stargazer)
 library(ggplot2)
+library(showtext)
+library(paletteer)
+
+
+# Enable showtext
+showtext_auto()
+
+# Add a Google Font
+font_add_google("Lato", "lato")
 
 sentiment_df <- read_csv("df_combined.csv")
+
 
 # t-test -----------------------------------------------------------
 
@@ -16,7 +26,9 @@ t.test(mean_sentiment ~ gender, data = sentiment_df)
 ols_model <- lm(mean_sentiment ~ gender * paper, data = sentiment_df)
 summary(ols_model)
 
-model_summary <- tidy(ols_model, conf.int = TRUE)
+model_summary <- tidy(ols_model, conf.int = TRUE, conf.level = 0.95)
+
+?tidy
 
 stargazer(ols_model, type = "html", out = "regression_results.doc")
 
@@ -33,21 +45,29 @@ ggplot(effect_plot) +
 sentiment_df$gender <- as.factor(sentiment_df$gender)
 sentiment_df$paper <- as.factor(sentiment_df$paper)
 
-interaction_results <- ggpredict(ols_model, terms = c("gender", "paper"))
+interaction_results <- ggpredict(ols_model, terms = c("gender", "paper"), ci_level = 0.95)
+
+?ggpredict
 
 print(interaction_results)
 
-ggplot(interaction_results, aes(x = x, y = predicted, group = group, color = group)) +
+selected_colors <- paletteer::paletteer_d("LaCroixColoR::Berry")[c(4, 6)]
+
+interaction <- ggplot(interaction_results, aes(x = x, y = predicted, group = group, color = group)) +
   geom_line(size = 1) +
   geom_point(size = 2) +
-  geom_errorbar(aes(ymin = conf.low, ymax = conf.high), width = 0.2, size = 0.4) +
+  scale_color_manual(values = selected_colors) +
+  geom_errorbar(aes(ymin = conf.low, ymax = conf.high), width = 0.2, size = 0.5) +
   labs(
-    title = "Interaction Effect of Gender and Paper on Sentiment",
-    x = "Gender",
+    title = NULL,
+    x = NULL,
     y = "Predicted Mean Sentiment Score",
-    color = "Paper"
+    color = "Source",
+    caption = "The interaction effect in shown with 95% confidence intervals."
   ) +
-  theme_minimal(base_family = "lato", base_size = 14)
+  theme_minimal(base_family = "lato", base_size = 30) 
+
+ggsave("interaction_plot.png", interaction, width = 6, height = 3, dpi = 300)
   
 
 # plotting the OLS results -----------  ------------------------------
