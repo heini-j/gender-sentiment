4fa8519d5f3e5b3a9507ada6f4b399bedd1dc0f0
Heini Järviö <heinimjarv@gmail.com>
2025-02-28 19:35:46 +0100

    cleaned code

--- 02 Statistical analysis/descriptives.R
+++ 02 Statistical analysis/descriptives.R
@@ -18,16 +18,44 @@ descriptives <- read_csv("df_combined.csv")
 
 # making a summary table of the data, with n of each code and mean_sentiment mean and sd for each code, sorted in descending order
 
-descriptives <- descriptives |>
-  mutate(across(starts_with("sentiment_"), as.numeric))
-
-descriptives_table <- descriptives |>
-  group_by(paper, gender) |>
+descriptives_paper <- descriptives |>
+  group_by(paper) |>
   summarise(n = n(),
             mean = mean(mean_sentiment, na.rm = TRUE),
-            sd = sd(mean_sentiment, na.rm = TRUE)
-  ) |>
-  arrange(desc(gender))
+            sd = sd(mean_sentiment, na.rm = TRUE),
+            mean_seniority = mean(seniority))
+
+descriptives_gender <- descriptives |>
+  group_by(gender) |>
+  summarise(n = n(),
+            mean = mean(mean_sentiment, na.rm = TRUE),
+            sd = sd(mean_sentiment, na.rm = TRUE),
+            mean_seniority = mean(seniority))
+
+descriptives_round <- descriptives |>
+  group_by(round) |>
+  summarise(n = n(),
+            mean = mean(mean_sentiment, na.rm = TRUE),
+            sd = sd(mean_sentiment, na.rm = TRUE),
+            mean_seniority = mean(seniority))
+
+descriptives_placement2024 <- descriptives |>
+  group_by(placement_2024) |>
+  summarise(n = n(),
+            mean = mean(mean_sentiment, na.rm = TRUE),
+            sd = sd(mean_sentiment, na.rm = TRUE),
+            mean_seniority = mean(seniority))
+
+descriptives_left_right <- descriptives |>
+  group_by(left_right) |>
+  summarise(n = n(),
+            mean = mean(mean_sentiment, na.rm = TRUE),
+            sd = sd(mean_sentiment, na.rm = TRUE),
+            mean_seniority = mean(seniority))
+
+descriptives_table <- bind_rows(list(descriptives_paper, descriptives_gender, 
+            descriptives_round, descriptives_placement2024, descriptives_left_right))
+
 
 write_excel_csv(descriptives_table, "descriptives_table.csv")
 
